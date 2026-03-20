f504000220985837c7590b28777b1555b1b1acc0
0470879-web <0470879@student.clayton.k12.ga.us>
2026-03-20 11:50:03 -0400

    Add vst-synths.js file with stock VST-like synthesizers

---
+++ vst-synths.js
@@ -0,0 +1,18 @@
+// vst-synths.js
+
+// Stock VST-like Synthesizers
+
+// Patcher
+function Patcher() {
+    // Code for Patcher emulation
+}
+
+// Sytrus
+function Sytrus() {
+    // Code for Sytrus emulation
+}
+
+// Sampler
+function Sampler() {
+    // Code for Sampler emulation
+}
